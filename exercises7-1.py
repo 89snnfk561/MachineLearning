28f2d24661f694985f6c62a0cc0b7a1e1f394124
89snnfk561 <kenny89002006@gmail.com>
2021-11-22 07:24:12 +0800

    exercises 7

--- exercises7-1.py
+++ exercises7-1.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def deriv_sigmoid(x):
     fx = sigmoid(x)
     return fx * (1-fx)
 
+
 def mse_loss(y_trues, y_preds):
     return ((y_trues - y_preds) ** 2).mean()
+
 
 
 class Neuron:
@@ -19,10 +23,11 @@ class Neuron:
         self.bias = bias
 
     def feedforward(self, inputs):
-        total = np.dot(self.weights, inputs) + self.bias
+        total = np.dot(inputs, self.weights) + self.bias
         return sigmoid(total)
 
-class NeuronNetwork:
+
+class NeuralNetwork:
 
     def __init__(self):
         weights = np.array([0.5, 0.5])
@@ -32,10 +37,11 @@ class NeuronNetwork:
         self.h2 = Neuron(weights, bias)
         self.o1 = Neuron(weights, bias)
 
-    def feedforword(self, inputs):
+    def feedforward(self, inputs):
         out_h1 = sigmoid(self.h1.feedforward(inputs))
         out_h2 = sigmoid(self.h2.feedforward(inputs))
-        out_o1 = sigmoid(self.o1.feedforward(np.array(out_h1, out_h2)))
+
+        out_o1 = sigmoid(self.o1.feedforward(np.vstack((out_h1, out_h2)).T))
         return out_o1
 
     def train(self, data, y_trues):
@@ -66,4 +72,56 @@ class NeuronNetwork:
                 d_ypred_d_h1 = self.o1.weights[0] * deriv_sigmoid(sum_o1)
                 d_ypred_d_h2 = self.o1.weights[1] * deriv_sigmoid(sum_o1)
 
+                # Neuron h1
+                d_h1_d_h1w1 = x[0] * deriv_sigmoid(sum_h1)
+                d_h1_d_h1w2 = x[1] * deriv_sigmoid(sum_h1)
+                d_h1_d_h1b1 = deriv_sigmoid(sum_h1)
 
+                # Neuron h2
+                d_h2_d_h2w1 = x[0] * deriv_sigmoid(sum_h2)
+                d_h2_d_h2w2 = x[1] * deriv_sigmoid(sum_h2)
+                d_h2_d_h2b1 = deriv_sigmoid(sum_h2)
+
+                # --- Update weights and biases
+                # Neuron h1
+                self.h1.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_h1w1
+                self.h1.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_h1w2
+                self.h1.bias -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_h1b1
+
+                # Neuron h2
+                self.h2.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_h2w1
+                self.h2.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_h2w2
+                self.h2.bias -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_h2b1
+
+                # Neuron o1
+                self.o1.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_o1w1
+                self.o1.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_o1w2
+                self.o1.bias -= learn_rate * d_L_d_ypred * d_ypred_d_o1b1
+
+            # --- Calculate total loss at the end of each epoch
+            if epoch % 10 == 0:
+                y_preds = np.apply_along_axis(self.feedforward, 1, data)
+                print(y_trues)
+                print(y_preds)
+
+                loss = mse_loss(y_trues, y_preds)
+                print("Epoch %d loss: %.3f" % (epoch, loss))
+
+# Define dataset
+data = np.array([
+  [-2, -1],  # Alice
+  [25, 6],   # Bob
+  [17, 4],   # Charlie
+  [-15, -6], # Diana
+])
+all_y_trues = np.array([
+  1, # Alice
+  0, # Bob
+  0, # Charlie
+  1, # Diana
+])
+
+# Train our neural network!
+network = NeuralNetwork()
+network.feedforward(data)
+network.train(data, all_y_trues)
